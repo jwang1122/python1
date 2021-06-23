9c4e9e45df29c004b9bb4886c96502d344ed289b
royboymaster <royboymaster1928@gmail.com>
2021-06-23 14:28:49 -0500

    turn in input02

---
+++ roy/input02.py
@@ -0,0 +1,11 @@
+
+firstName=input("please enter your first name:")
+lastName=input("please enter your last name:")
+# print ( "Hello", lastName + "," , firstName + "!")
+print(f"Hello {lastName}, {firstName}!" )
+
+
+
+
+
+
