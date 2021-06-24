14b788f6ebbcc0ca657bf12ad23f5a0404e2347b
jwang1122 <jwang1122@gmail.com>
2021-06-24 15:42:42 -0500

    end of lesson 8

--- src/set.py
+++ src/set.py
@@ -69,6 +69,20 @@ print(set2)
 print(set3)
 set3 = set1 | set2
 print(set3)
-# set management (union)
+set1 = {1,2,3}
+set2 = {1,2,3}
+print(set1>set2) # set1 has every element in set2, and set1>set2
+print(set1<set2)
+print(set1==set2)
 
-# Checking relation between sets
+# function for set(union, update, difference, intersection, isdisjoint)
+set2={3,4,5}
+set3 = set1.union(set2)
+print(set1)
+print(set2)
+print(set3)
+set3 = set1.difference(set2)
+set3 = set2.difference(set1)
+print(set3)
+set3 = set1.intersection(set2)
+print(set3)
