b95f47f74dfc430633936ff0c28aab9cd779c4ac
macmagine <macmagine@gmail.com>
2021-06-23 19:01:27 -0500

    message

--- src/set.py
+++ src/set.py
@@ -45,10 +45,25 @@ print(set1)
 # set3.pop()
 set1.remove(11)
 print(set1)
+# set1.remove(11)
 
+# operator on set
+# &: intersection operator
+# |: union
+set1 = set(range(1, 6))
+set2 = set(range(3, 8))
+set3 = set1 & set2
+print(set1)
+print(set2)
+print(f"intersection of set1 and set2 is {set3}")
 
 # relationship between sets
 set1 = {1,2,3,4,5}
 set2 = {4,5,6,7,8}
 set3 = set1.intersection(set2)
+print(set3)
+
+# set3 = set1 + set2
+# print(set3)
+set3 = set1 | set2
 print(set3)
