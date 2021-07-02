d22a7b96afce86ddf5a6be38853461d875c99562
royboymaster <royboymaster1928@gmail.com>
2021-07-02 11:18:03 -0500

    math06 I think cylinder sirface area formula is wrong

---
+++ roy/math06.py
@@ -0,0 +1,11 @@
+from math import pi
+r = 4
+h = 6
+surfacearea= 2*pi*r + h*(2*pi*r**2)
+volume = pi*r**2*h
+print(f"Height of Cylinder: {h}")
+print(f"Radius of cylinder: {r}")
+print()
+print(f"volume is: {volume}") 
+print(f"Surface Area is:{surfacearea}")
+
