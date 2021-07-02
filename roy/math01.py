b52d240294a7f7cd40db8dc529eecc5d924e32d6
royboymaster <royboymaster1928@gmail.com>
2021-07-02 10:42:43 -0500

    maht 01 figured  out

--- roy/math01.py
+++ roy/math01.py
@@ -1,4 +1,4 @@
 
-radius = input("radius of circle:")
+radius = 3
 x=(radius*2)
 print(f"The circle diameter with radius={radius} is",(x))
---
+++ roy/math03.py
@@ -0,0 +1,5 @@
+from math import pi
+radius=3
+diameter=radius*2
+circumference=diameter*pi
+print(f"the circle circumference with a radius={radius} is {circumference}" )
