2d7b1289bee6083762210a5b0d3738745d6b1a72
royboymaster <royboymaster1928@gmail.com>
2021-07-02 10:17:45 -0500

    How to do please answer in QA

--- roy/math01.py
+++
@@ -1,3 +0,0 @@
-radius = 3
-
-print(f"The circle diameter with radius={radius} is",(radius*2))
---
+++ roy/qa/math01.py
@@ -0,0 +1,4 @@
+
+radius = input("radius of circle:")
+x=(radius*2)
+print(f"The circle diameter with radius={radius} is",(x))
