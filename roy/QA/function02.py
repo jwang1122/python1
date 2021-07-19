e55d65f6143e5b00e8f6af80123953d9b11d27d2
royboymaster <royboymaster1928@gmail.com>
2021-07-19 16:13:28 -0500

    function 02 final

--- roy/QA/function02.py
+++ roy/QA/function02.py
@@ -1,6 +1,14 @@
-x = {"a": 1, "b": 2}
-y = {"b": 3, "c": 4}
-z = {**y, **x}
-print(z)
 
+def mergeDict(x, y):\
+    # since **x is put as the second argument, so if there is a same key 
+    # in both dictionary, the one in x will be prioritized
+    return{**y,**x} 
+
+
+
+if __name__ == '__main__':
+    x = {"a": 1, "b": 2}
+    y = {"b": 3, "c": 4}
+    z = mergeDict(x,y)
+    print(f"z = {z}")
 
