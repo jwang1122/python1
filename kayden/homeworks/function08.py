bdde7efd8a72fadd5f78aeea1648c486e3dbc22c
auderyh <auderyh@yahoo.com>
2021-07-19 17:26:10 -0500

    send

--- kayden/homeworks/function08.py
+++ kayden/homeworks/function08.py
@@ -18,3 +18,17 @@ def func1(items):
 
 func1([1,2,3,4])
 
+
+print()
+
+def func1(*args):
+    print(f"func1{args}")
+    for i in args:
+        print(i)
+
+if __name__ == '__main__':
+    func1(20, 40, 60)
+    print()
+    func1(80, 100, 'hello', 'kaydenthedude', (1,2,3), ["h", "j"])
+
+
---
+++ kayden/homeworks/function11.py
@@ -0,0 +1,10 @@
+def outerFunc(a, b):
+    def innerFunc():
+        return a + b
+    return innerFunc() + 5
+
+if __name__ == '__main__':
+    a = 5
+    b = 10
+    x = outerFunc(a, b)
+    print(x)
